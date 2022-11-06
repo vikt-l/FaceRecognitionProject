c82efecff6675bc71e1f1a791fcc67650e2005d8
vikt-l <viktorialesnyh87@gmail.com>
2022-11-06 16:20:07 +0300

    add face recognition

--- code/main.py
+++ code/main.py
@@ -1,15 +1,19 @@
 import sys
-import sqlite3
-from PIL import Image
-
-#import face_recognition as fr
 import cv2
+import numpy
+import sqlite3
+import os
 
-from code.mainWindow import Ui_MainWindow
+import face_recognition as fr
+
+from mainWindow import Ui_MainWindow
 from widgetAddPerson import PersonInfoAdd
 from getHelpWidget import Help
-from PyQt5 import QtCore, QtWidgets
+from func import f_addVideotodb
+
 from PyQt5.Qt import *
+from PyQt5 import QtCore, QtWidgets, QtMultimedia
+from PyQt5.QtCore import Qt
 
 if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
     QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
@@ -19,7 +23,6 @@ if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
 
 
 class Form(QMainWindow, Ui_MainWindow):
-    # главная форма
 
     def __init__(self):
         super().__init__()
@@ -28,30 +31,46 @@ class Form(QMainWindow, Ui_MainWindow):
 
     def initUI(self):
         self.theme = 'light'
-
-        # self.btn_start = QtWidgets.QPushButton(self)
-        # self.btn_start.setText('start')
-        # self.btn_start.clicked.connect(self.start)
-
-
-        self.addPeople.clicked.connect(self.f_addpeople)
-        self.dateTimeEd.setDateTime(QDateTime.currentDateTime())
+        self.flag_recording = False
+        self.number_recording = 0
 
         self.btn_theme.clicked.connect(self.changeColor)
-        self.setLightTheme()
-
-        self.cBoxNames.activated.connect(self.get_info)
+        self.addPeople.clicked.connect(self.f_addpeople)
         self.btn_help.clicked.connect(self.get_help)
+        self.btnRecording.clicked.connect(self.start_recording)
+        self.btnStopRecording.clicked.connect(self.stop_recording)
 
+        self.addPeople.clicked.connect(self.load_mp3)
+        self.btnRecording.clicked.connect(self.load_mp3)
+        self.btnStopRecording.clicked.connect(self.load_mp3)
+        self.btn_theme.clicked.connect(self.load_mp3)
+        self.btn_help.clicked.connect(self.load_mp3)
 
-    def start(self):
+        self.setLightTheme()
+        self.dateTimeEd.setDateTime(QDateTime.currentDateTime())
+        self.cBoxNames.activated.connect(self.get_info)
+
+        self.thread = ThreadOpenCV()
         self.thread.start()
+        self.thread.changePixmap.connect(self.setVideo)
+        self.thread.changePixmap.connect(self.recording)
 
-    def f_addpeople(self):
-        # создает форму для довабления информации о человеке в базу данных
+    def setVideo(self, img, names, peoples, count):
+        self.video.setPixmap(QPixmap.fromImage(img))
 
-        self.form_to_add_people = PersonInfoAdd(self.theme)
-        self.form_to_add_people.show()
+        self.names = names
+        self.peoples = peoples
+        self.count = count
+
+        self.cBoxNames.clear()
+
+        if self.flag_recording:
+            for i in names:
+                self.all_peoples.add(i)
+
+        self.lcdNumberPeople.display(count)
+        self.namesPeoples.setPlainText(peoples)
+        self.cBoxNames.addItems(names)
 
     def changeColor(self):
         # меняет тему приложения
@@ -77,7 +96,7 @@ class Form(QMainWindow, Ui_MainWindow):
         self.namesPeoples.setStyleSheet('color: #0b1016; background-color: #cacfd5')
         self.infoPeople.setStyleSheet('color: #0b1016; background-color: #cacfd5')
         self.dateTimeEd.setStyleSheet('color: #0b1016; background-color: #cacfd5')
-        self.lcdNumberPeople.setStyleSheet('background-color: #cacfd5')
+        self.lcdNumberPeople.setStyleSheet('background-color: #697278')
         self.lbl_date.setStyleSheet('color: #0b1016')
         self.lblNumberPeople.setStyleSheet('color: #0b1016')
 
@@ -99,19 +118,60 @@ class Form(QMainWindow, Ui_MainWindow):
         self.lbl_date.setStyleSheet('color: #f0f2f3')
         self.lblNumberPeople.setStyleSheet('color: #f0f2f3')
 
-    def set_video(self, image):
-        # добавление видео и информации на форму
+    def get_help(self):
+        # открывает форму для получения описания программы
 
-        self.namesPeoples.clear()
-        self.infoPeople.clear()
-        self.img_photo.clear()
-        self.cBoxNames.clear()
+        self.window_help = Help()
+        self.window_help.show()
 
-        # self.lcdNumberPeople.display(find_faces)
-        # self.namesPeoples.setPlainText(names)
-        # self.cBoxNames.addItems(peoples)
+    def f_addpeople(self):
+        # создает форму для довабления информации о человеке в базу данных
 
-        self.video.setPixmap(QPixmap.fromImage(image))
+        self.form_to_add_people = PersonInfoAdd(self.theme)
+        self.form_to_add_people.show()
+
+    def start_recording(self):
+        self.dt_rec = self.dateTimeEd.dateTime()
+        self.all_peoples = set()
+        self.count_not_known = 0
+        self.flag_recording = True
+        self.number_recording += 1
+
+        frame_width = 700
+        frame_height = 700
+        fourcc = cv2.VideoWriter_fourcc(*'MPEG')
+        self.video_record = cv2.VideoWriter(f'../recording_video/recording_{self.number_recording}.avi', fourcc,
+                                            20.0, (frame_width, frame_height))
+
+    def recording(self, *args):
+        if self.flag_recording:
+            args = list(args)
+            self.video_record.write(args[-1])
+
+    def stop_recording(self):
+        if self.flag_recording:
+            self.flag_recording = False
+
+            f_addVideotodb(self.dt_rec, self.all_peoples,
+                           f'../recording_video/recording_{self.number_recording}.avi')
+        else:
+            pass
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Q:
+            self.f_addpeople()
+
+        elif event.key() == Qt.Key_W:
+            self.start_recording()
+
+        elif event.key() == Qt.Key_E:
+            self.stop_recording()
+
+        elif event.key() == Qt.Key_R:
+            self.get_help()
+
+        elif event.key() == Qt.Key_T:
+            self.changeColor()
 
     def get_info(self):
         # добавляет на форму информацию о выбранном человеке
@@ -120,158 +180,88 @@ class Form(QMainWindow, Ui_MainWindow):
         name = n[0]
         surname = n[1]
 
-        con = sqlite3.connect('person_db.sqlite')
+        con = sqlite3.connect('../person_db.sqlite')
         cur = con.cursor()
-        res = cur.execute(f"""SELECT * FROM person
-        WHERE name LIKE '{name}' AND surname LIKE '{surname}'""").fetchall()
-        print(res)
+        res = cur.execute(f"""select * from person
+        where name like '{name}' and surname like '{surname}'""").fetchall()
+        con.close()
 
-        age, year, information, photoPath = res[0]
+        age, year, information, photoPath = tuple(list(res[0])[3:])
         self.infoPeople.setPlainText(f'{name} {surname}\nвозраст: {age}, дата рождения: {year}\n{information}')
 
-        photo = Image.open(photoPath)
-        fixed_height = 150
-        percent = fixed_height / float(photo.size[0])
-        width_size = int((float(photo.size[1]) * float(percent)))
-        img = photo.resize((fixed_height, width_size))
-        img.save('result.jpeg')
+        img = QImage(photoPath).scaled(200, 200, Qt.KeepAspectRatio)
+        self.img_photo.setPixmap(QPixmap.fromImage(img))
 
-        self.pixmap = QPixmap('result.jpeg')
-        self.lbl_photo.setPixmap(self.pixmap)
-
-        self.img_photo.setPixmap(photo)
-
-    def start_recording(self):
-        # начало записи видео
-
-        self.sp_peoples = set()
-        self.count_not_known = 0
-        self.flag_recording = True
-        self.number_recording += 1
-        self.dt_rec = self.dateTimeEd.dateTime()
-
-        frame_width = int(self.cap.get(3))
-        frame_height = int(self.cap.get(4))
-
-        self.video_recording = cv2.VideoWriter(f'../recording_video/recording_{self.number_recording}.avi', fourcc,
-                                               20.0, (frame_width, frame_height))
-
-    def stop_recording(self):
-        # остановка записи видео и внесение информации в базу данных
-
-        self.flag_recording = False
-        f_addVideotodb(1, self.sp_peoples, self.count_not_known,
-                       f'../recording_video/recording_{self.number_recording}.avi')
-
-    def get_help(self):
-        # открывает форму для получения описания программы
-
-        self.window_help = Help()
-        self.window_help.show()
+    def load_mp3(self):
+        media = QtCore.QUrl.fromLocalFile('../Sound_17216.mp3')
+        content = QtMultimedia.QMediaContent(media)
+        self.player = QtMultimedia.QMediaPlayer()
+        self.player.setMedia(content)
+        self.player.play()
 
 
 class ThreadOpenCV(QThread):
-    changePixmap = pyqtSignal(QImage)
+    changePixmap = pyqtSignal(QImage, list, str, int, numpy.ndarray)
 
     def __init__(self):
         super().__init__()
-        self.flag_recording = False
-        self.peoples = []
-        self.number_recording = 0
 
-    def video_run(self):
+    def run(self):
         cap = cv2.VideoCapture(0)
+        known_people = os.listdir('../people')
 
         while True:
-            ret, frame = cap.read()
-            if ret:
+            success, frame = cap.read()
+            if success:
+
+                cv2.imwrite('../photo/image.jpeg', frame)
+                img_fr = fr.load_image_file('../photo/image.jpeg')
+                faces_loc = fr.face_locations(img_fr)
+                find_faces = len(faces_loc)
+                names = []
+                peoples = ''
+
+                for i in range(find_faces):
+                    y, x1, y1, x = faces_loc[i]
+                    cv2.imwrite('../photo/image_face.jpeg', frame[y:y1, x:x1])
+                    cv2.rectangle(frame, (x, y), (x1, y1), (250, 250, 0), 2)
+
+                    for i_face in known_people:
+                        known_face = fr.load_image_file(f'../people/{i_face}')
+                        known_face_enc = fr.face_encodings(known_face)[0]
+
+                        unknown_face = fr.load_image_file('../photo/image_face.jpeg')
+                        unknown_face_enc = fr.face_encodings(unknown_face)[0]
+
+                        result = fr.compare_faces([known_face_enc], unknown_face_enc)
+
+                        if result:
+                            name = i_face[:i_face.find('.')].split('_')[0]
+                            surname = i_face[:i_face.find('.')].split('_')[1]
+                            cv2.putText(frame, f"{name} {surname}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                                        (20, 20, 0), 2,
+                                        cv2.LINE_AA)
+                            names.append(f"{name} {surname}")
+
+                        if not result:
+                            cv2.putText(frame, 'unknown', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (200, 200, 0), 1,
+                                        cv2.LINE_AA)
+
+                    peoples = '\n'.join(names)
+                    if len(names) < find_faces:
+                        peoples += f'неизвестные: {find_faces - len(names)}'
+
                 rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 h, w, ch = rgbImage.shape
                 bytesPerLine = ch * w
                 convertToQtFormat = QImage(
                     rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888)
-                p = convertToQtFormat.scaled(300, 300, Qt.KeepAspectRatio)
-                self.changePixmap.emit(p, 'hello')
+                p = convertToQtFormat.scaled(700, 700, Qt.KeepAspectRatio)
+                self.changePixmap.emit(p, names, peoples, find_faces, frame)
 
             self.msleep(20)
-
-    # def video_run(self):
-    #     # получение доступа к камере, поиск и идентификация лиц
-    #
-    #     # вывести find_facec, image, name - surname/ unknown
-    #
-    #     cap = cv2.VideoCapture('../test_media/video.mp4')
-    #     known_people = os.listdir('../people')
-    #
-    #     while cap.isOpened():
-    #
-    #         success, self.img = cap.read()
-    #         if not success:
-    #             if self.flag_recording:
-    #                 self.stop_recording()
-    #             break
-    #
-    #         cv2.imwrite('../photo/image.jpeg', self.img)
-    #
-    #         img_fr = fr.load_image_file('../photo/image.jpeg')
-    #         faces_loc = fr.face_locations(img_fr)
-    #         find_faces = len(faces_loc)
-    #
-    #         self.current_k_Npeople = 0
-    #
-    #         for i in range(find_faces):
-    #
-    #             y, x1, y1, x = faces_loc[i]
-    #             cv2.imwrite('../photo/image_face.jpeg', self.img[y:y1, x:x1])
-    #             cv2.rectangle(self.img, (x, y), (x1, y1), (250, 250, 0), 2)
-    #             result = False
-    #
-    #             for i_face in known_people:
-    #                 known_face = fr.load_image_file(f'../people/{i_face}')
-    #                 known_face_enc = fr.face_encodings(known_face)[0]
-    #
-    #                 unknown_face = fr.load_image_file('../photo/image_face.jpeg')
-    #                 unknown_face_enc = fr.face_encodings(unknown_face)[0]
-    #
-    #                 result = fr.compare_faces([known_face_enc], unknown_face_enc)
-    #
-    #                 if result:
-    #                     name = i_face[:i_face.find('.')].split('_')[0]
-    #                     surname = i_face[:i_face.find('.')].split('_')[1]
-    #                     cv2.putText(self.img, f"{name} {surname}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1,
-    #                                 (20, 20, 0), 2,
-    #                                 cv2.LINE_AA)
-    #                     self.peoples.append(f"{name} {surname}")
-    #                     #
-    #                     # if self.flag_recording:
-    #                     #     self.sp_peoples.add(f"{name} {surname}")
-    #                     break
-    #
-    #             if not result:
-    #                 cv2.putText(self.img, 'unknown', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (200, 200, 0), 1,
-    #                             cv2.LINE_AA)
-    #
-    #                 self.current_k_Npeople += 1
-    #                 #
-    #                 # if self.flag_recording:
-    #                 #     self.count_not_known += 1
-    #
-    #         self.names = '\n'.join(self.peoples)
-    #         if self.current_k_Npeople:
-    #             self.names += f"\nнеизвестные: {self.current_k_Npeople}"
-    #
-    #         cv2.imwrite('../photo/image_2.jpeg', self.img)
-    #
-    #         rgbImage = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
-    #         h, w, ch = rgbImage.shape
-    #         bytesPerLine = ch * w
-    #         convertToQtFormat = QImage(
-    #             rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888)
-    #         p = convertToQtFormat.scaled(500, 500, Qt.KeepAspectRatio)
-    #         self.changePixmap.emit(p, find_faces, self.peoples, self.names)
-    #
-    #         # if self.flag_recording:
-    #         #     self.video_recording.write(self.img)
+        cap.realease()
+        cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
